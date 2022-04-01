22d4963715c4f33dac5fccaa4903314aafa0cc01
TSREACT <98829018+TSREACT@users.noreply.github.com>
2022-04-01 16:50:11 +0100

    Deployment

--- slashCommand.js
+++ slashCommand.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser');
 const subjects = require('./elements/subjects.json')
+const cors = require('cors')
 
 module.exports.listenForCommands = async function (app) {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
-
+  app.use(cors())
+  
   app.post('/commands', (req, res) => {
     const { token, user_id, channel_id } = req.body
     console.log(`Received a slash command from user ${user_id} in channel ${channel_id}`)
